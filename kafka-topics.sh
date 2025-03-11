1f8de0d4f174bc5979e595b29dc6c17bd53aecfd
Jhoel Acosta <jhoel9211@gmail.com>
2025-03-10 19:52:07 -0500

    Add user-notifications Kafka topic

--- kafka-topics.sh
+++ kafka-topics.sh
@@ -1,2 +1,3 @@
 docker exec -it gp-kafka-infra-kafka kafka-topics --create --topic game-events --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1
 docker exec -it gp-kafka-infra-kafka kafka-topics --create --topic social-events --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1
+docker exec -it gp-kafka-infra-kafka kafka-topics --create --topic user-notifications --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1
